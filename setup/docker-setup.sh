17667759b9a60ad2f1abe810a9c78c5a16dc6e57
Jaehyun Nam <namjh@kaist.ac.kr>
2018-09-20 03:18:34 +0900

    minor updates

--- setup/docker-setup.sh
+++ setup/docker-setup.sh
@@ -8,7 +8,7 @@ $DOCKER_HOME/docker-clean.sh
 
 # create new containers
 
-NUM_OF_DOCKERS=32
+NUM_OF_DOCKERS=31
 
 $DOCKER_HOME/script/1_run_docker.sh $NUM_OF_DOCKERS
 
@@ -31,10 +31,10 @@ $DOCKER_HOME/script/2_create_ovs.sh $CONTROLLERS $PROTOCOL $NIC1 $NIC2 $NIC3 $NI
 # link all containers to the bridge
 
 # NETWORK(X.X.X).DOCKER(1 ~ $NUM_OF_DOCKERS)
-NETWORK=172.16.0
-START_IP=33
-END_IP=64
+NETWORK=172.16.10
+START_IP=2
+END_IP=32
 CIDR=24
-GATEWAY=172.16.0.1
+GATEWAY=172.16.10.1
 
 $DOCKER_HOME/script/3_connect_docker_to_ovs.sh $NETWORK $START_IP $END_IP $CIDR $GATEWAY
